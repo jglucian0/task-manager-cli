3f4b23e9ac5a3061076fb5565e27b6d2bc60c2ad
João Gabriel <jgluciano.luz@gmail.com>
2025-07-03 09:50:44 -0300

    feat: adiciona opção de filtro por status e prioridade no menu interativo

--- interface/menu.py
+++ interface/menu.py
@@ -9,6 +9,7 @@ def exibir_menu():
     print("[3] Concluir tarefa")
     print("[4] Editar tarefa")
     print("[5] Remover tarefa")
+    print("[6] Filtrar tarefas")
     print("[0] Sair")
     print("====================================")
 
@@ -31,11 +32,15 @@ def menu():
                 nova_tarefa = capturar_dados_tarefa()
                 gerenciador.adicionar_taefa(nova_tarefa)
             elif escolha == '2':
+                gerenciador.limpar_tela()
+                print('\n📋  Tarefas cadastradas:')
                 gerenciador.listar_tarefa()
             elif escolha == '3':
+                gerenciador.limpar_tela()
+                print('\n📋  Tarefas cadastradas:')
                 if not gerenciador.tarefas:
                     print('\n⚠️  Nenhuma tarefa cadastrada.')
-                    gerenciador.limpar_tela()
+                    gerenciador.limpar_tela_e_pause()
                 else:
                     gerenciador.listar_tarefa()
                     try:
@@ -46,9 +51,11 @@ def menu():
                         print('\n⚠️  Entra iválida.')
                         gerenciador.limpar_tela()
             elif escolha == '4':
+                gerenciador.limpar_tela()
+                print('\n📋  Tarefas cadastradas:')
                 if not gerenciador.tarefas:
                     print('\n⚠️  Nenhuma tarefa cadastrada.')
-                    gerenciador.limpar_tela()
+                    gerenciador.limpar_tela_e_pause()
                 else:
                     gerenciador.listar_tarefa()
                     try:
@@ -67,14 +74,16 @@ def menu():
                                 indice, titulo, data, prioridade)
                         else:
                             print('\n⚠️  Entrada iválida.')
-                            gerenciador.limpar_tela()
+                            gerenciador.limpar_tela_e_pause()
                     except (ValueError, TypeError):
                         print('\n⚠️  Entrada iválida.')
-                        gerenciador.limpar_tela()
+                        gerenciador.limpar_tela_e_pause()
             elif escolha == '5':
+                gerenciador.limpar_tela()
+                print('\n📋  Tarefas cadastradas:')
                 if not gerenciador.tarefas:
                     print('\n⚠️  Nenhuma tarefa cadastrada.')
-                    gerenciador.limpar_tela()
+                    gerenciador.limpar_tela_e_pause()
                 else:
                     gerenciador.listar_tarefa()
                     try:
@@ -84,22 +93,43 @@ def menu():
                             gerenciador.remover_tarefa(indice)
                         else:
                             ('\n⚠️  Entrada iválida.')
-                            gerenciador.limpar_tela()
+                            gerenciador.limpar_tela_e_pause()
                     except (ValueError, TypeError):
                         print('\n⚠️  Entrada iválida.')
-                        gerenciador.limpar_tela()
+                        gerenciador.limpar_tela_e_pause()
+            elif escolha == '6':
+                gerenciador.limpar_tela()
+                print("\n[1] Tarefas pendentes")
+                print("[2] Tarefas concluídas")
+                print("[3] Filtrar por prioridade")
+                filtro = input("Escolha uma opção de filtro: ")
+
+                if filtro == '1':
+                    gerenciador.limpar_tela()
+                    print('\n📋  Tarefas pendentes:')
+                    gerenciador.filtrar_pendentes()
+                elif filtro == '2':
+                    gerenciador.limpar_tela()
+                    print('\n📋  Tarefas concluídas:')
+                    gerenciador.filtrar_concluidas()
+                elif filtro == '3':
+                    gerenciador.limpar_tela()
+                    gerenciador.filtrar_prioridade()
+                else:
+                    print('\n⚠️  Opção iválida, tente novamente.')
+                    gerenciador.limpar_tela_e_pause()
             elif escolha == '0':
                 print('\n👋 Saindo do programa...')
-                gerenciador.limpar_tela()
+                gerenciador.limpar_tela_e_pause()
                 break
             else:
                 print('\n⚠️  Opção iválida, tente novamente.')
-                gerenciador.limpar_tela()
+                gerenciador.limpar_tela_e_pause()
         except (ValueError, TypeError):
             print('\n⚠️  Entrada iválida.')
-            gerenciador.limpar_tela()
+            gerenciador.limpar_tela_e_pause()
         except KeyboardInterrupt:
             print('\n⚠️  Usuário interrompeu o programa.')
             print('👋 Saindo do programa...')
-            gerenciador.limpar_tela()
+            gerenciador.limpar_tela_e_pause()
             break
--- system/manager.py
+++ system/manager.py
@@ -13,47 +13,98 @@ class TaskManager:
     def adicionar_taefa(self, task):
         self.tarefas.append(task)
         self.salvar()
-        print('\n✅ Tarefa cadastrada com sucesso!')
         self.limpar_tela()
+        print('\n✅ Tarefa cadastrada com sucesso!')
+        self.limpar_tela_e_pause()
 
     def remover_tarefa(self, indice):
         if 0 <= indice < len(self.tarefas):
             removida = self.tarefas.pop(indice)
             self.salvar()
-            print(f'\n✅ Tarefa [{removida.titulo}] removida com sucesso!')
             self.limpar_tela()
+            print(f'\n✅ Tarefa [{removida.titulo}] removida com sucesso!')
+            self.limpar_tela_e_pause()
         else:
             print('\n⚠️  Índice iválido.')
-            self.limpar_tela()
+            self.limpar_tela_e_pause()
 
     def listar_tarefa(self):
         if not self.tarefas:
             print('\n⚠️  Nenhuma tarefa cadastrada.')
-            self.limpar_tela()
+            self.limpar_tela_e_pause()
             return
         print('')
         for i, tarefa in enumerate(self.tarefas, 1):
             print(f'{i} - {tarefa}')
 
+    def filtrar_pendentes(self):
+        if not self.tarefas:
+            print('\n⚠️  Nenhuma tarefa cadastrada.')
+            self.limpar_tela_e_pause()
+            return
+        print('')
+        pendentes = [tarefa for tarefa in self.tarefas if not tarefa.concluido]
+        for i, tarefa in enumerate(pendentes, 1):
+            print(f'{i} - {tarefa}')
+
+    def filtrar_concluidas(self):
+        if not self.tarefas:
+            print('\n⚠️  Nenhuma tarefa cadastrada.')
+            self.limpar_tela_e_pause()
+            return
+        print('')
+        concluidas = [tarefa for tarefa in self.tarefas if tarefa.concluido]
+        if not concluidas:
+            print('⚠️  Nenhuma tarefa concluída.')
+            self.limpar_tela_e_pause()
+            return
+        for i, tarefa in enumerate(concluidas, 1):
+            print(f'{i} - {tarefa}')
+
+    def filtrar_prioridade(self):
+        if not self.tarefas:
+            print('\n⚠️  Nenhuma tarefa cadastrada.')
+            self.limpar_tela_e_pause()
+            return
+        print('')
+        try:
+            prioridade_input = int(input("Digite a prioridade (1-5): "))
+            if 0 <= prioridade_input <= 5:
+                filtro_prioridade = [
+                    tarefa for tarefa in self.tarefas if tarefa.prioridade == prioridade_input]
+                self.limpar_tela()
+                print(
+                    f'\n📋  Tarefas filtradas por prioridade nível ({prioridade_input}):')
+                print('')
+                for i, tarefa in enumerate(filtro_prioridade, 1):
+                    print(f'{i} - {tarefa}')
+            else:
+                print('\n⚠️  Valor de prioridade iválida.')
+                return
+        except (TypeError, ValueError):
+            print('\n⚠️  Entrada iválida.')
+
     def editar_tarefa(self, indice, titulo=None, data_limite=None, prioridade=None):
         if 0 <= indice < len(self.tarefas):
             self.tarefas[indice].editar(titulo, data_limite, prioridade)
             self.salvar()
-            print('\n✅ Tarefa atualizada com sucesso!')
             self.limpar_tela()
+            print('\n✅ Tarefa atualizada com sucesso!')
+            self.limpar_tela_e_pause()
         else:
             print('\n⚠️  Índice iválido.')
-            self.limpar_tela()
+            self.limpar_tela_e_pause()
 
     def concluir_tarefa(self, indice):
         if 0 <= indice < len(self.tarefas):
             self.tarefas[indice].concluir()
             self.salvar()
-            print('\n✅ Tarefa marcada como concluída.')
             self.limpar_tela()
+            print('\n✅ Tarefa marcada como concluída.')
+            self.limpar_tela_e_pause()
         else:
             print('\n⚠️  Índice iválido.')
-            self.limpar_tela()
+            self.limpar_tela_e_pause()
 
     def salvar(self):
         with open(self.caminho_arquivo, 'w', encoding='utf-8') as arquivo:
@@ -70,10 +121,13 @@ class TaskManager:
                 print('\n⚠️  Erro ao carregar arquivo. Inexistente ou corrompido.')
                 self.tarefas = []
                 self.salvar()
-                self.limpar_tela()
+                self.limpar_tela_e_pause()
         else:
             self.tarefas = []
 
     def limpar_tela(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    def limpar_tela_e_pause(self):
         sleep(2)
         os.system('cls' if os.name == 'nt' else 'clear')
